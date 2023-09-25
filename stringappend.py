16d2395908fd16d7e62334a824e4b7d157ad745c
Navya-135 <143674020+Navya-135@users.noreply.github.com>
2023-09-25 15:18:53 +0530

    Add files via upload

---
+++ stringappend.py
@@ -0,0 +1,18 @@
+#Operator overloading
+
+fName="Rajesh"
+middleName="Rana Pratap"
+lName="Varma"
+print("fullName:"+ fName + " " + middleName + " " + lName)
+
+#f'string/Interpolation
+fName="Rajesh"
+middleName="Rana Pratap"
+lName="Varma"
+print("fullName:" f"{fName} {middleName} {lName}" )
+
+#String join method
+fName="Rajesh"
+middleName="Rana Pratap"
+lName="Varma"
+print("fullName:",fName,middleName,lName)
